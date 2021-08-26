8fa12c29db4d68d602e815e1079990a275d04781
andmalt <andrea.maltempi@gmail.com>
2021-08-26 17:40:03 +0200

    inserito metodo mer maiuscole e console per indicare in quale posizione stà il nostro cognome

--- Js/myscript.js
+++ Js/myscript.js
@@ -4,26 +4,29 @@ let cognomiArray = ["Bianchi","Neri","Rossi","Verdi","Gialli"];
 
 // chiediamo all'utente il suo cognome
 
-let cognome = prompt("Inserisci il tuo cognome");
+let cognome = (prompt("Inserisci il tuo cognome").toUpperCase());
+
 
 // inseriamo il cognome nell'array 
 
 cognomiArray.push(cognome);
 
+
 // ordiniamo l'array alfabeticamente
 
 cognomiArray.sort();
 
+
 // inseriamo gli array in ordine numerico partendo da 1
 
-for( let i = 1; i <= cognomiArray.length; i++){
-    console.log(i+" "+cognomiArray[i-1]);
+
+for ( let i = 1; i <= cognomiArray.length; i++){
+    console.log(i+" "+cognomiArray[i-1]); 
 }
 
+//  indichiamo in quale posizione è stato inserito il nostro cognome
 
-
-
-
+console.log(cognomiArray.indexOf(cognome)+1);
 
 
 
